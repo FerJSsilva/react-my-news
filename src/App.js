4d02b9b5de19395d36f2ef23b5ef8cd190b3ffc0
Fernando Silva <ferjssilva@gmail.com>
2019-10-22 23:55:41 -0300

    refactor: remove unnecessary components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  console.log('environment', process.env.REACT_APP_NEWS_API_KEY);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>{process.env.REACT_APP_NEWS_API_KEY}</span>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>My News</span>
     </div>
   );
 }
